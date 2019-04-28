709d96b29cbdc1817a03922e5460d4730af570f3
Ediz Turcan <ediz.turcan@icloud.com>
2019-04-28 13:08:57 +0200

    default user creation

--- database/database.go
+++ database/database.go
@@ -6,8 +6,11 @@ import (
 	"database/sql"
 	"log"
 	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
+// SetupDatabase Creates the database if there is no one
 func SetupDatabase() {
 	if _, err := os.Stat(globals.Config.Database); os.IsNotExist(err) {
 		log.Println("No database found. Creating...")
@@ -23,6 +26,15 @@ func SetupDatabase() {
 		tools.CheckError(err)
 		insertUserRoles.Exec(globals.RoleAdmin.ID, globals.RoleAdmin.Description)
 		insertUserRoles.Exec(globals.RoleUser.ID, globals.RoleUser.Description)
+
+		// Create User
+		createUser, err := db.Prepare("CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, firstname VARCHAR(255) NOT NULL, lastname VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, role INTEGER NOT NULL, FOREIGN KEY (role) REFERENCES user_role(id));")
+		tools.CheckError(err)
+		createUser.Exec()
+		passwordHash, err := bcrypt.GenerateFromPassword([]byte("private"+globals.Config.Pepper), bcrypt.DefaultCost)
+		insertuser, err := db.Prepare("INSERT INTO user (name, password, firstname, lastname, email, role) VALUES (?, ?, ?, ?, ?, ?)")
+		tools.CheckError(err)
+		insertuser.Exec("admin", string(passwordHash), "Admin", "Almighty", "admin@allnetworks.com", globals.RoleAdmin.ID)
 
 	} else {
 		log.Println("Loading database...")
